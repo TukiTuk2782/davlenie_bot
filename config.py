e46be50cbbcf548a2cdc3777f82962a10f5dc80a
AU_findir <au@findi.io>
2025-12-28 20:31:54 +0300

    config updated

--- config.py
+++ config.py
@@ -1,17 +1,12 @@
 import os
 from dotenv import load_dotenv
 
-# load_dotenv()
-#
-# BOT_TOKEN = os.getenv(BOT_TOKEN)
-# SPREADSHEET_ID = os.getenv(SPREADSHEET_ID)
-# SERVICE_ACCOUNT_FILE = os.getenv(GOOGLE_SERVICE_ACCOUNT_FILE)
+load_dotenv()
 
-
-
-BOT_TOKEN = "8504421553:AAFyzGugHGxWdFz4j9x87facB-zX_3sVdwg"
-SPREADSHEET_ID = "1bddN_z_xUEsha0kNc1fFaLO8RYTwzCxmoXrMh7Ycms0"
-SERVICE_ACCOUNT_FILE = "service_account.json"
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+SPREADSHEET_ID = os.getenv("SPREADSHEET_ID")
+SERVICE_ACCOUNT_FILE = os.getenv("SERVICE_ACCOUNT_FILE")
+print(f"SERVICE_ACCOUNT_FILE - {SERVICE_ACCOUNT_FILE}")
 
 
 if BOT_TOKEN is None:
